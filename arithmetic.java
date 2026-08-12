17183b92ca67c94ee355ce160a8584b926bf283e
aishwarya <aishwaryakarthika2008@gmail.com>
2026-08-12 23:09:00 +0530

    Create arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,11 @@
+class Arithmetic {
+    public static void main(String[] aishu) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
