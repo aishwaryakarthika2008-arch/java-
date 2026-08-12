01fb1189c7a5cd550ffbba07ce73cc70db626404
aishwarya <aishwaryakarthika2008@gmail.com>
2026-08-12 22:47:59 +0530

    Create Typecasting.java

---
+++ Typecasting.java
@@ -0,0 +1,13 @@
+class Typecasting{
+    public static void main(String[] args){
+        
+        int age = 19;
+        double mark = age;
+        System.out.println(mark);
+         
+        double value = 59.7;
+        int number = (int) value;
+        System.out.println(number);
+
+    }
+}
