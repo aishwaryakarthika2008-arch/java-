9f93ac1b18df727c2f05683a7e6b038895a1a80a
aishwarya <aishwaryakarthika2008@gmail.com>
2026-08-12 23:12:56 +0530

    Create arithmeticInput.java

---
+++ arithmeticInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Arithmetic {
+    public static void main(String[] aishu) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
