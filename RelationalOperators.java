127666d7956b1649dad5c099a4d7fe35039719bd
aishwarya <aishwaryakarthika2008@gmail.com>
2026-08-13 22:16:11 +0530

    Create RelationalOperators.java

---
+++ RelationalOperators.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class RelationalOperators{
+    public static void main(String[] ars){
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println(k==n);
+        System.out.println(k!=n);
+        System.out.println(k<n);
+        System.out.println(k>n);
+        System.out.println(k<=n);
+        System.out.println(k>=n);
+    }
+}
