bd087cb02a7fb004799953f889f816eb7b63b18f
aishwarya <aishwaryakarthika2008@gmail.com>
2026-08-13 22:03:22 +0530

    Implement assignment operators in AssignmentOperators.java

---
+++ AssignmentOperators.java
@@ -0,0 +1,10 @@
+class AssignmentOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a+=5);
+        System.out.println(a-=6);
+        System.out.println(a*=9);
+        System.out.println(a%=8);
+        System.out.println(a/10);
+    }
+}
