f40b63df9afe910adb1b76320af72116d2bd5be1
aishwarya <aishwaryakarthika2008@gmail.com>
2026-08-13 22:43:11 +0530

    Add PreDecrement class to demonstrate pre-decrement

---
+++ PreDecrement.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class PreDecrement{
+    public static void main(String[] args){
+  Scanner sc = new Scanner(System.in);
+  int a = sc.nextInt();
+  System.out.print(--a);
+}
+}
