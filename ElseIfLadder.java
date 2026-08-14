db509543a6be98899654a75550a023f8182f9022
aishwarya karunanidhi <aishwaryakarthika2008@gmail.com>
2026-08-14 23:09:05 +0530

    Add ElseIfLadder class for grading system

---
+++ ElseIfLadder.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ElseIfLadder{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int mark = sc.nextInt();
+if (mark >= 90) {
+    System.out.println("A");
+}
+else if (mark >= 75) {
+    System.out.println("B");
+}
+else if (mark >= 50) {
+    System.out.println("C");
+}
+else {
+    System.out.println("Fail");
+}
+}
+}
