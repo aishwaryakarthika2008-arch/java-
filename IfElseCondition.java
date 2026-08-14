e311cb6fa11c28e77534b0fbe887accd6ef81589
aishwarya karunanidhi <aishwaryakarthika2008@gmail.com>
2026-08-14 22:38:19 +0530

    Add IfElseCondition class for pass/fail evaluation

---
+++ IfElseCondition.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class IfElseCondition{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    if(a>=40)
+    System.out.println("Pass");
+    else
+    System.out.println("Fail");
+    }
+}
