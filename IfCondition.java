60c7b2c84e09a807fc81050fd698d0c16585f722
aishwarya <aishwaryakarthika2008@gmail.com>
2026-08-14 22:32:02 +0530

    Create IfCondition.java

---
+++ IfCondition.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class IfCondition{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    if(a>=18)
+    System.out.println("Eligible");
+    }
+}
